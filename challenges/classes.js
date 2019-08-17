6024896e5ee6a4712d8f59bee842ba5941a5793b
aleesha-wood <spectrat@spectratart.com>
2019-08-16 23:26:34 -0400

    stretch completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,7 +4,7 @@ console.log("-----------------------CLASS CONVERSION----------------------------
 class ConvertCuboidMaker{
   constructor(cuboid) {
   this.length = cuboid.length;
-  this.width = cuboid.width;
+  this.width =  cuboid.width;
   this.height = cuboid.height;
   }
   volume() {
@@ -25,7 +25,7 @@ class ConvertCuboidMaker{
 
 const convertCuboid = new CuboidMaker({
   length: 4,
-  width: 5,
+  width:  5,
   height: 5
 });
 
@@ -34,9 +34,25 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+console.log("--------------------------STRECH-------------------------------------")
 
 class CubeMaker extends CuboidMaker{
   constructor(cube){
     super(cube);
   }
+  cubeVolume() {
+    return (Math.pow(this.length, 3));
+  }
+  cubeSurfaceArea() {
+    return 6 * (Math.pow(this.length, 2));
+  }
 }
+
+const cube = new CubeMaker({
+  length: 12,
+  width:  12,
+  height: 12
+});
+
+console.log(cube.cubeVolume());
+console.log(cube.cubeSurfaceArea());
